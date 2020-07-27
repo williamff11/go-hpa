20681d7486688b03aaaf26941716b1f0c8c5d0c4
williamfreire <williamff11@gmail.com>
2020-07-27 09:55:24 -0300

    hpa

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	x := 0.0001;
+	x := 0.0001
 	for i := 0; i <= 1000000; i++ {
-		x += math.Sqrt(x);
-		fmt.Println("Code.education Rocks!")
+		x += math.Sqrt(x)
 	}
+	fmt.Println("Code.education Rocks!")
 }
